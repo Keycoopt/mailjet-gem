266eb6d70cc63dfcf179587fe7718fccf5671308
Guillaume Charneau <guillaume@keycoopt.com>
2016-04-22 15:07:45 +0200

    WIP

---
+++ spec/mailjet/mailer_spec.rb
@@ -0,0 +1,10 @@
+#rspec
+require 'mailjet_spec_helper'
+
+describe Mailjet::APIMailer do
+  describe "#deliver!" do
+    describe "The payload sent to Mailjet::MessageDelivery contains both a :from_name and a :from_email keys" do
+      
+    end
+  end
+end
